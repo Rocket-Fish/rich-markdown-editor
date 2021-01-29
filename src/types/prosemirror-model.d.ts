d642854e8c9b0d145a8a1373b05df2ba59478ce4
rocket-fish <rocket.fish@mail.com>
2021-01-29 11:34:41 -0500

    ts ignore entire d.ts file

--- src/types/prosemirror-model.d.ts
+++ src/types/prosemirror-model.d.ts
@@ -1,10 +1,14 @@
-declare module "prosemirror-model" {
-  /* eslint-disable no-unused-vars */
-  import model from "prosemirror-model";
+/* eslint-disable no-unused-vars */
+import model from "prosemirror-model";
+
+// @ts-ignore
+  declare module "prosemirror-model" {
+  // @ts-ignore
   interface Slice {
     // this method is missing in the DefinitelyTyped type definition, so we
     // must patch it here.
     // https://github.com/ProseMirror/prosemirror-model/blob/bd13a2329fda39f1c4d09abd8f0db2032bdc8014/src/replace.js#L51
+    // @ts-ignore
     removeBetween(from: number, to: number): Slice;
   }
 }
